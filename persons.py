0c0d3952037c8dcea93593e722c46bb6df9dd5b2
55NUTS <34411665+polaris2010@users.noreply.github.com>
2024-09-17 23:24:33 +0400

    Add files via upload

---
+++ persons.py
@@ -0,0 +1,39 @@
+from string import ascii_letters
+
+
+class Person:
+    S_RUS = 'абвгдеёжзиклмнопрстуфхцчшщъыьэюя-'
+    S_RUS_UPPER = S_RUS.upper()
+
+    def __init__(self, fio, old, ps, weight):
+        self.verify_fio(fio)
+        self.verify_old(old)
+        self.verify_ps(ps)
+        self.verify_weight(weight)
+
+        self.__fio = fio.split()
+        self.__old = old
+        self.__passport = ps
+        self.__weight = weight
+
+    @classmethod
+    def verify_fio(cls, fio):
+        if type(fio) != str:
+            raise TypeError("ФИО должно быть строкой")
+
+        f = fio.split()
+        if len(f) != 3:
+            raise TypeError("Неверный формат ФИО")
+
+        letters = ascii_letters + cls.S_RUS + cls.S_RUS_UPPER
+        for s in f:
+            if len(s) < 1:
+                raise TypeError("В ФИО должен быть хотя бы один символ")
+            if len(s.strip(letters)) != 0:
+                raise TypeError("В ФИО должны быть только буквы")
+
+
+
+
+
+
